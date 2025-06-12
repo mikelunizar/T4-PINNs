b806938a3f71312e78f577333079ce40ca64ea9c
mikelmartinez <mikel.martinez@unizar.es>
2025-06-12 12:03:06 +0200

    added run.sh and run.sub

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source /mnt/cephfs/home/amb/mikel.martinez/miniconda3/bin/activate mesh2
+#python  /mnt/cephfs/home/amb/mikel.martinez/meshgraph/mesh-graph/train.py
+wandb agent amb_zgz/BurgerPINN/5uos8csd
