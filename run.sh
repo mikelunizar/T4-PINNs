b7a55753f093d87f1d3b314ef33034bf11d3f20a
mikelmartinez <mikel.martinez@unizar.es>
2025-06-12 12:39:46 +0200

    updated config file and run.sh with sweep

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 
 source /mnt/cephfs/home/amb/mikel.martinez/miniconda3/bin/activate mesh2
 #python  /mnt/cephfs/home/amb/mikel.martinez/meshgraph/mesh-graph/train.py
-wandb agent amb_zgz/BurgerPINN/5uos8csd
+wandb agent amb_zgz/HyperSearcBurgerPINN/vqqxcv39
