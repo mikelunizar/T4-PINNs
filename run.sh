513828c1445728a8e4aaf42254996951351d3f54
mikelmartinez <mikel.martinez@unizar.es>
2025-06-30 11:55:00 +0200

    modified notebook and added sweep to run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,5 @@
 
 source /mnt/cephfs/home/amb/mikel.martinez/miniconda3/bin/activate mesh2
 #python  /mnt/cephfs/home/amb/mikel.martinez/meshgraph/mesh-graph/train_pendulum.py
-wandb agent amb_zgz/HyperSearcBurgerPINN/vqqxcv39
+ wandb agent amb_zgz/BurgerBestConvergence/rzcqkgz4
+
