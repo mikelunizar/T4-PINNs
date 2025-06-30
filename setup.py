9e1c8bdef8aeb6c57e902e455286cd3ab8b4824e
mikelmartinez <mikel.martinez@unizar.es>
2025-06-30 12:05:22 +0200

    added setup installer

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pinn_lightning',            # Replace with your package name
+    version='0.1.0',
+    author='Mikel M Iparraguirre',
+    author_email='mikel.martinez@unizar.es',
+    description='PDE solver based on PINNs Neural Networks in Lightning',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/mikelunizar/T4-PINNs',  # Replace with your repo URL
+    packages=find_packages(),  # Automatically find all packages and subpackages
+    install_requires=[
+        # Add your required dependencies here
+        'torch',
+        'numpy',
+        'matplotlib',
+        'pytorch-lightning',
+        'wandb',
+        'matplotlib',
+        'scikit-learn',
+        'imageio',
+        'jupyter'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
